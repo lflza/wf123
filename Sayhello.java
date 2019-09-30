3bc588ca34b15f2a3bc601642a2f713375fb25eb
unknown <1274951447@qq.com>
2019-09-30 17:28:04 +0800

    wufeisb

---
+++ Sayhello.java
@@ -0,0 +1,15 @@
+public class Sayhello{
+    public static void main (String[] args){
+	    System.out.println("Hello tianhanyao");
+		    Sayhello(sum());
+			}
+			public static void sayhello (int sum){
+			System.out.println('sayhello Hello!" + sum)£»
+			}
+			public static void sayHello(int sum){
+			int sum=0;
+			for (int i = 0;i<= 100;i++}{
+			          sum = sum +i;
+                    }
+            return sum;
+              }			
